05599f81e46b2c0874745f574e33a0d56cd6df18
younglherme <guilhermeluizartes@hotmail.com>
2026-05-11 10:13:58 -0300

    commit inicial

---
+++ src/main/java/dev/guilhermesilva/atom_backend/AtomBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.guilhermesilva.atom_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtomBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/guilhermesilva/atom_backend/AtomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guilhermesilva.atom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
